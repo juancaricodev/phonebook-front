c19267b239c3f44b00981ce20d2deaeb658f52f8
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-12 23:13:57 -0500

    feat(context): context created; pending implementation in containers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import FetchContext from './context/FetchContext'
 
 import App from './routes/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <FetchContext>
+    <App />
+  </FetchContext>,
+  document.getElementById('root')
+)
