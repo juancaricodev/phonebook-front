ab8dc388ad19f203cab7ded4b191e485c55c6f73
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-15 16:05:08 -0500

    feat(index.js, DataContext): components cleared

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -4,45 +4,6 @@ import phonebookService from '@services/phonebook'
 
 export const DataContext = React.createContext({})
 
-// const initialData = [
-//   {
-//     name: 'Arto Hellas',
-//     number: '040-123456',
-//     id: 1,
-//     deleted: false
-//   },
-//   {
-//     name: 'Ada Lovelace',
-//     number: '39-44-5323523',
-//     id: 2,
-//     deleted: false
-//   },
-//   {
-//     name: 'Dan Abramov',
-//     number: '12-43-234345',
-//     id: 3,
-//     deleted: false
-//   },
-//   {
-//     name: 'Mary Poppendieck',
-//     number: '39-23-6423122',
-//     id: 4,
-//     deleted: false
-//   },
-//   {
-//     id: 5,
-//     name: 'Camilo',
-//     number: '12345',
-//     deleted: false
-//   },
-//   {
-//     id: 6,
-//     name: 'Carlos',
-//     number: '2345423',
-//     deleted: true
-//   }
-// ]
-
 export const DataContextProvider = ({ children }) => {
   const [persons, setPersons] = useState([])
 
@@ -60,7 +21,6 @@ export const DataContextProvider = ({ children }) => {
       .deleteId(id, deletedPerson)
       .then((newData) => {
         setPersons(persons.map(person => (person.id !== id ? person : newData)))
-        // filteredData.length > 0 && setFilteredData(filteredData.map(person => (person.id !== id ? person : newData)))
       })
       .catch(err => console.log(`Error deleting person with id ${id} => ${err}`))
 
