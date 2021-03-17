ec1f87bdd3f7e4dd3cb18b079b0d1808d18166db
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-17 17:37:47 -0500

    feat(test): setup-tests.js config file created

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,9 @@
+// import React from 'react'
+import Layout from '../layouts/Layout/index.jsx'
+import { mount } from 'enzyme'
+
+test('Rendering layout component', () => {
+  const wrapper = mount(
+    <Layout />
+  )
+})
