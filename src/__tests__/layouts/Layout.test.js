ef583ef9628aeb9b1262dffa141549075bdb1cf9
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-17 21:47:49 -0500

    feat(jest): webpack alias implemented in jest config

--- src/__tests__/layouts/Layout.test.js
+++ src/__tests__/layouts/Layout.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import Layout from '../../layouts/Layout'
-import Header from '../../layouts/Layout/components/Header'
+import Layout from '@layouts/Layout'
+import Header from '@layouts/Layout/components/Header'
 import { mount, render } from 'enzyme'
 
 describe('<Layout >/', () => {
