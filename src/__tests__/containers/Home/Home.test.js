58992e70961165b9dad92ded4ea08a104f29d92c
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-18 13:58:02 -0500

    test(Home): test updated for rendering title and description

--- src/__tests__/containers/Home/Home.test.js
+++ src/__tests__/containers/Home/Home.test.js
@@ -5,9 +5,20 @@ import { mount } from "enzyme"
 const home = mount(<Home />)
 
 describe('<Home />', () => {
-  test('Rendering Home container', () => {
+  test('Home render', () => {
     const findHome = home.find('.home')
     expect(findHome.length).toEqual(1)
     expect(findHome.text()).toBe('I\'m Home')
+  })
+
+  test('Home title render', () => {
+    const homeTitle = home.find('.home-title')
+
+    expect(homeTitle.length).toEqual(1)
+    expect(homeTitle.text()).toBe('PhoneBook')
+  })
+
+  test('Home description render', () => {
+    expect(home.find('.home-description').length)toEqual(1)
   })
 })
