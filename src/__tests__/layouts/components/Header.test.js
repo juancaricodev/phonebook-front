8a21353090208d73e2df989c632a027ebd656930
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-18 09:39:55 -0500

    test(Header): test fixed using shallow

--- src/__tests__/layouts/components/Header.test.js
+++ src/__tests__/layouts/components/Header.test.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '@layouts/Layout/components/Header'
-import { mount } from 'enzyme'
+import { shallow } from 'enzyme'
 
 describe('<Header />', () => {
-  const header = mount(<Header />)
+  const header = shallow(<Header />)
 
   test('Rendering Header', () => {
     expect(header.length).toEqual(1)
+  })
+
+  test('Header navbar (.header className)'\, () => {
+    expect(header.find('.header').length).toEqual(1)
   })
 })
