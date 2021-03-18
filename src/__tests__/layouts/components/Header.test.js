7db9bce07f3bb42c1cac8412be1e26fa28f2785c
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-18 12:51:49 -0500

    test(Header): testing route links

--- src/__tests__/layouts/components/Header.test.js
+++ src/__tests__/layouts/components/Header.test.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import Header from '@layouts/Layout/components/Header'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
+import { Link, MemoryRouter } from 'react-router-dom'
 
 describe('<Header />', () => {
-  const header = shallow(<Header />)
+  const header = mount(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>
+  )
 
   test('Rendering Header', () => {
     expect(header.length).toEqual(1)
   })
 
-  test('Header navbar (.header className)'\, () => {
+  test('Header navbar (.header className)', () => {
     expect(header.find('.header').length).toEqual(1)
+  })
+
+  test('Route links', () => {
+    const routes = header.find(Link).map(link => link.prop('to'))
+    expect(routes[0]).toBe('/')
+    expect(routes[1]).toBe('/phonebook')
+    expect(routes[2]).toBe('/new-person')
   })
 })
