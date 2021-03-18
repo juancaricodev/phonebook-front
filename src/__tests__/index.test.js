476f360ebc9733573f600fec9d21905ec037e1cf
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-17 20:01:45 -0500

    test(Layout): test created

--- src/__tests__/index.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Layout from '../layouts/Layout/index.jsx'
-import { mount } from 'enzyme'
-
-test('Rendering layout component', () => {
-  const wrapper = mount(
-    <Layout />
-  )
-})
