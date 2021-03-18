32a4fe9407fb66fb97824f6b7ef4932766a0d211
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-17 20:30:45 -0500

    test(Layout): test updated

--- src/__tests__/Layout.test.js
+++ src/__tests__/Layout.test.js
@@ -3,10 +3,13 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Layout from '../layouts/Layout/index.jsx'
 import { mount } from 'enzyme'
 
-test('Rendering layout component', () => {
-  const wrapper = mount(
-    <Router>
-      <Layout />
-    </Router>
-  )
+describe('<Layout >/', () => {
+  test('Rendering Layout component', () => {
+    const wrapper = mount(
+      <Router>
+        <Layout />
+      </Router>
+    )
+    expect(wrapper.length).toEqual(1)
+  })
 })
