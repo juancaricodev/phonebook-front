b0bad4a8f3541a8440b46dc8466c0fdb808b09d6
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-18 22:08:02 -0500

    test(Phonebook): test for container render and title implemented

--- src/__tests__/containers/Phonebook/Phonebook.test.js
+++ src/__tests__/containers/Phonebook/Phonebook.test.js
@@ -1,7 +1,18 @@
-import { mount } from "enzyme"
+import React from 'react'
 import Phonebook from "@containers/Phonebook"
+import { mount } from "enzyme"
 
 describe('<Phonebook />', () => {
   const phoneBook = mount(<Phonebook />)
 
+  test('Container render', () => {
+    expect(phoneBook.find('.phonebook').length).toEqual(1)
+  })
+
+  test('Title', () => {
+    const title = phoneBook.find('h1')
+
+    expect(title.length).toEqual(1)
+    expect(title.text()).toBe('Phonebook')
+  })
 })
