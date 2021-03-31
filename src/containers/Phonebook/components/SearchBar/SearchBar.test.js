3f84121cfd2ea8f31d5a47fbd38e5ea15bd9e078
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-30 23:08:49 -0500

    test(SearchBar): initial test content added

---
+++ src/containers/Phonebook/components/SearchBar/SearchBar.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import SearchBar from '@containers/Phonebook/components/SearchBar'
+
+
+describe('<SearchBar />', () => {
+  const searchBar = phoneBook.find(SearchBar)
+
+  test('Container render', () => {
+    expect(searchBar.length).toEqual(1)
+  })
+
+  test('Input render', () => {
+    expect(searchBar.find('input').prop('placeholder')).toBe('Search by name')
+  })
+
+  test('Button render', () => {
+    const button = searchBar.find('button')
+
+    expect(button.length).toEqual(1)
+    expect(button.prop('type')).toBe('submit')
+    expect(button.text()).toBe('Search')
+  })
+})
